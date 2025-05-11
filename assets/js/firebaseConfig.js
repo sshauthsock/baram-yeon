ed392938d0fbb573ea298919105cbbec36c74b07
dklim-multiplay <dk.lim@unity3d.com>
2025-05-11 11:42:11 +0900

    add page counter

--- assets/js/firebaseConfig.js
+++ assets/js/firebaseConfig.js
@@ -7,3 +7,15 @@ const firebaseConfig = {
   appId: "1:924298156656:web:845c94e771625fbd24b2b5",
   measurementId: "G-F2BT2T7HCL",
 };
+
+const firebaseCountConfig = {
+  apiKey: "AIzaSyBWBbe8carOdeIzP6hQsarDOz5H0TuEj9A",
+  authDomain: "baram-yeon.firebaseapp.com",
+  databaseURL:
+    "https://baram-yeon-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "baram-yeon",
+  storageBucket: "baram-yeon.firebasestorage.app",
+  messagingSenderId: "924298156656",
+  appId: "1:924298156656:web:845c94e771625fbd24b2b5",
+  measurementId: "G-F2BT2T7HCL",
+};
