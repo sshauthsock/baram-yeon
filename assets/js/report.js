4fa8b6379429d61317c2a4dd5c390374d8c41a48
dklim-multiplay <dk.lim@unity3d.com>
2025-05-25 14:28:23 +0900

    modified report link

---
+++ assets/js/report.js
@@ -0,0 +1,9 @@
+const Report = (function () {
+  function openReportSheet() {
+    window.open("https://forms.gle/v1F41Dq2GxhbJKmBA", "_blank");
+  }
+
+  return {
+    openReportSheet,
+  };
+})();
